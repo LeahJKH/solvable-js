ac79b39e78fd9098976250a020e36bb4b850e6ad
LeahJKH <leahjb06.kodehode@gmail.com>
2024-04-17 12:15:31 +0200

    doing some page changes

--- js/siteLogic.js
+++ js/siteLogic.js
@@ -3,7 +3,11 @@
 
 //dette er bare så dere får feedback på oppgaven
 
-
+const pageEl = {
+    headerT: document.querySelector("#top-box"),
+    maincont: document.querySelector("#main-cont"),
+    footerB: document.querySelector("#btm-box")
+}
 
 
 
@@ -21,16 +25,18 @@ const oppg1 = false
 const oppg2 = false
 const oppg3 = false
 const oppg4 = false
+const oppg5 = false
 
-import { string, number, object, boolean, array, oppgave4, oppgave3, myFunc  } from "./index.js"
+import { string, number, object, boolean, array, oppgave4, oppgave3, myFunc, oppgave5  } from "./index.js"
 if(string === "per knut" && number === 23 && object.navn === "Nina" && object.alder === 42
 && array[0] === "skog" && array[1] === "vann" && array[2] === "mark" && boolean === true) {
     const div = document.createElement("div")
     const txt = document.createTextNode("godt jobbet :3 oppgave 1 løst")
     const p = document.createElement("p")
+    
     p.append(txt)
-    div.appendChild(p                                                                                                              )
-    document.body.appendChild(div) 
+    div.appendChild(p)                                                                                                             
+    pageEl.maincont.appendChild(div) 
     oppg1 = !oppg1
 } else {
     console.log("prøv noe nytt på oppgave 1")
@@ -45,7 +51,7 @@ const fNFT = myFunc(sNFT)
         const p = document.createElement("p")
         p.append(txt)
         div.appendChild(p)
-        document.body.appendChild(div) 
+        pageEl.maincont.appendChild(div) 
         oppg2 = !oppg2
     } else {
         console.log("prøv noe nytt på oppgave 2")
@@ -60,7 +66,7 @@ if (oppg1 && oppg2) {
         const p = document.createElement("p")
         p.append(txt)
         div.appendChild(p)
-        document.body.appendChild(div)
+        pageEl.maincont.appendChild(div) 
         oppg3 = !oppg3 
    } else {
     console.log("prøv noe nytt på oppgave 3")
@@ -74,9 +80,23 @@ if (oppg1 && oppg2 && oppg3) {
         const p = document.createElement("p")
         p.append(txt)
         div.appendChild(p)
-        document.body.appendChild(div)
+        pageEl.maincont.appendChild(div) 
         oppg4 = !oppg4 
     } else {
         console.log("prøv noe nytt på oppgave 4")
+    }
+}
+if (oppg1 && oppg2 && oppg3 && oppg4) {
+    let finishedNum = oppgave5()
+    if (finishedNum === 640) {
+        const div = document.createElement("div")
+        const txt = document.createTextNode("godt jobbet :3 oppgave 5 løst")
+        const p = document.createElement("p")
+        p.append(txt)
+        div.appendChild(p)
+        pageEl.maincont.appendChild(div) 
+        oppg5 = !oppg5 
+    } else {
+        console.log("prøv noe nytt på oppgave 5")
     }
 }
